39f35c4ef49cf830a93a96c66994e3632e64155d
thejaswinidm <thejaswini360@gmail.com>
2019-04-10 09:52:36 +0530

    x

--- test1.py
+++ test1.py
@@ -1,18 +1,74 @@
 from ant_colony import Ant_colony 
 test_nodes = {0: (0, 7), 1: (3, 9), 2: (12, 4), 3: (14, 11), 4: (8, 11), 5: (15, 6), 6: (6, 15), 7: (15, 9), 8: (12, 10), 9: (10, 7)}
+n = int(input("enter the number of processes"))
+# test_nodes={}
+print("Enter the arriving time and burst time of all the process")
+# for i in range(n):
+# 	temp=raw_input().split(",")
+# 	test_nodes[i]=(int(temp[0]),int(temp[1]))
 
-# //...and a function to get distance between nodes...
 def distance(start, end):
 	x_distance = abs(start[0] - end[0])
 	y_distance = abs(start[1] - end[1])
 	
-	# //c = sqrt(a^2 + b^2)
 	import math
 	return math.sqrt(pow(x_distance, 2) + pow(y_distance, 2))
 
-# //...we can make a colony of ants...
+# we can make a colony of ants...
 colony = Ant_colony(test_nodes, distance)
 
-# //...that will find the optimal solution with ACO
+#that will find the optimal solution with ACO
 answer = colony.mainloop()
+print("**************************************")
+print("The optimal sequence is : ")
 print(answer)
+
+
+def waitingTime(i):
+	t=0
+	time=0
+	total=0
+	for j in range(n):
+		temp=time-test_nodes[answer[t]][0]
+		print "waiting time for process ",t ," is ",temp
+		time +=test_nodes[answer[t]][1]
+		total+=temp
+		t+=1
+	return total
+
+def turnAroundTime():
+	t=0
+	time=0
+	total=0
+	for j in range(n):
+		temp=time-test_nodes[answer[t]][0]+test_nodes[answer[t]][1]
+		print "turn around time of process ",t," is " ,temp
+		time +=test_nodes[answer[t]][1]
+		total+=temp
+		t+=1
+	# t=0
+	# time=0
+	# for j in range(n):
+	# 	startTime=test_nodes[answer[t]][0]
+	# 	burst=test_nodes[answer[t]][1]
+	# 	if time < startTime:
+	# 		time =startTime+burst
+	# 		print(burst)
+	# 	else:
+	# 		time += burst
+	# 		print(time-startTime)
+
+	# 	#time +=test_nodes[answer[t]][1]
+	# 	#time -= test_nodes[answer[t]][0]
+	# 	t+=1
+	return total
+
+avgWaitingTime = waitingTime(0)/n
+print("**************************************")
+print("average waiting time :")
+print(avgWaitingTime)
+print("**************************************")
+avgTurnAround = turnAroundTime()/n
+print("**************************************")
+print("average turn around time :")
+print(avgTurnAround)
